f2b5b87f2eee49c8b63e11e818c51f233ca26336
Jerry12138 <448216252@qq.com>
2020-07-08 00:46:32 +0800

    提交 2020.07.08

--- src/app.js
+++ src/app.js
@@ -11,12 +11,27 @@ const redisStore = require('koa-redis');
 
 const { REDIS_CONF } = require('./conf/db');
 const { SESSION_SECRET_KEY } = require('./conf/secretKeys');
+const { isProd } = require('./utils/env');
 
-const index = require('./routes/index');
-const users = require('./routes/users');
+// 路由
+const atAPIRouter = require('./routes/api/blog-at');
+const squareAPIRouter = require('./routes/api/blog-square');
+const profileAPIRouter = require('./routes/api/blog-profile');
+const homeAPIRouter = require('./routes/api/blog-home');
+const blogViewRouter = require('./routes/view/blog');
+const utilsAPIRouter = require('./routes/api/utils');
+const userViewRouter = require('./routes/view/user');
+const userAPIRouter = require('./routes/api/user');
+const errorViewRouter = require('./routes/view/error');
 
 // error handler
-onerror(app);
+let onerrorConf = {};
+if (isProd) {
+  onerrorConf = {
+    redirect: '/error',
+  };
+}
+onerror(app, onerrorConf);
 
 // middlewares
 app.use(bodyparser({
@@ -54,8 +69,15 @@ app.use(session({
 }));
 
 // routes
-app.use(index.routes(), index.allowedMethods());
-app.use(users.routes(), users.allowedMethods());
+app.use(atAPIRouter.routes(), atAPIRouter.allowedMethods());
+app.use(squareAPIRouter.routes(), squareAPIRouter.allowedMethods());
+app.use(profileAPIRouter.routes(), profileAPIRouter.allowedMethods());
+app.use(homeAPIRouter.routes(), homeAPIRouter.allowedMethods());
+app.use(blogViewRouter.routes(), blogViewRouter.allowedMethods());
+app.use(utilsAPIRouter.routes(), utilsAPIRouter.allowedMethods());
+app.use(userAPIRouter.routes(), userAPIRouter.allowedMethods());
+app.use(userViewRouter.routes(), userViewRouter.allowedMethods());
+app.use(errorViewRouter.routes(), errorViewRouter.allowedMethods()); // 404 路由注册到最后面
 
 // error-handling
 app.on('error', (err, ctx) => {
